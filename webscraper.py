fc17781d8b145a781bc2a79bc56c6b4bc6feb636
Max Gustaw <109792316+OGGEE61@users.noreply.github.com>
2023-03-29 20:16:32 +0200

    Add files via upload

---
+++ webscraper.py
@@ -0,0 +1,21 @@
+import requests
+import bs4
+base_url = 'http://books.toscrape.com/catalogue/page-{}.html'
+
+
+two_star_title = []
+for n in range(1,51):
+    
+    scrape_url = base_url.format(n)
+    res = requests.get(scrape_url)
+    
+    soup = bs4.BeautifulSoup(res.text,'lxml')
+    books = soup.select('.product_pod')
+    
+    for book in books:
+        
+        if len(book.select('.star-rating.Two')) !=0:
+            book_title = book.select('a')[1]['title']
+            two_star_title.append(book_title)
+
+    print(two_star_title)
